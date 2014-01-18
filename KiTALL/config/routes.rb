d9ecd2d994cce758e90d04a5fed2e46c3fb1bef2
Gaurav <gauravmi@thoughtworks.com>
2014-01-18 14:54:14 +0530

    Created blank activity wall page <Gaurav/Jaideep>

--- KiTALL/config/routes.rb
+++ KiTALL/config/routes.rb
@@ -1,4 +1,5 @@
 KiTALL::Application.routes.draw do
+	get "activity" => "users#activity_wall"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
