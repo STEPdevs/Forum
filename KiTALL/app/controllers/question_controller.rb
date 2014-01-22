4cc761f9170997d8ffab00c12bdc59ebe5180613
Ravi <ravis4376@gmail.com>
2014-01-22 19:59:34 +0530

    failed failing tests --Ravi

---
+++ KiTALL/app/controllers/question_controller.rb
@@ -0,0 +1,4 @@
+class QuestionController  < ApplicationController
+
+  
+end
---
+++ KiTALL/spec/routes/question_routes_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe QuestionController do
+
+  #it 'should route /question/new' do
+  #  {:post => 'question/new'}.should route({:controller => 'question',
+  #                                             :action => 'new'})
+  #end
+
+end
